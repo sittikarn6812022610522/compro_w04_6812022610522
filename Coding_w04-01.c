0ae59ef75c0412921ca55faef5ac5e2c095fed05
sittikarn <s6812022610522@email.kmutnb.ac.th>
2025-07-16 16:53:05 +0700

    เห้อออ

--- Coding_w04-01.c
+++ Coding_w04-01.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include<stdio.h>               // #include คือ keyword, <stdio.h> คือ library
 
-int main() {
-    printf("Hello,\n");
-    printf("C Tokens!");
-    return 0;
-}
+int main() {                    // int คือ keyword, main คือ identifier, () คือ operator, { คือ operator
+    printf("Hello,\n");         //paintf คือ identifier, Hello, \n คือ constant, ; คือ operator
+    printf("C Tokens!");        //paintf คือ identifier, C Tokens! คือ constant, ; คือ operator
+    return 0;                   // return คือ keyword, 0 คือ constant, ; คือ operator
+}                               // } คือ operator
